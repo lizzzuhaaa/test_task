4e474f10bb4c6e87481a7e2f2b79acaabce2bde6
Sydorova Liza <yelizavieta.sydorova@ukma.edu.ua>
2025-04-09 16:25:28 +0300

    Initial Commit

---
+++ Test_task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_task
+//
+//  Created by лізушка лізушкіна on 09.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .blue
+    }
+
+
+}
+
