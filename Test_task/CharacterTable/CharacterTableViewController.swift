b1f3be2d3de3ea9709f15f8605456467d1b27d05
Sydorova Liza <yelizavieta.sydorova@ukma.edu.ua>
2025-04-09 20:31:44 +0300

    API managing

--- Test_task/CharacterTable/CharacterTableViewController.swift
+++ Test_task/CharacterTable/CharacterTableViewController.swift
@@ -11,6 +11,9 @@ class CharacterTableViewController: UIViewController, UITableViewDelegate, UITab
     
     //MARK: Properties
     var tableView = UITableView()
+    
+    var networkManager = APIManager()
+    var characters: Array<Character> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +26,34 @@ class CharacterTableViewController: UIViewController, UITableViewDelegate, UITab
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
+        
+        Task{
+            do{
+                let loadedList = try await networkManager.getCharactersList()
+                characters = loadedList
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch{
+                print(error.localizedDescription)
+            }
+            
+        }
+    
     }
     
     // MARK: Settings for rows count
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return characters.count
     }
     
     // MARK: Settings for rows info
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let character = characters[indexPath.row]
         cell.selectionStyle = .gray
-        cell.textLabel?.text = "cell"
-        cell.imageView?.image = UIImage(systemName: "circle")
+        cell.textLabel?.text = character.name
+        cell.imageView?.image = character.image
         return cell
     }
 }
---
+++ Test_task/Services/APIManager.swift
@@ -0,0 +1,79 @@
+//
+//  NetworkManager.swift
+//  Test_task
+//
+//  Created by лізушка лізушкіна on 09.04.2025.
+//
+
+import Foundation
+import UIKit
+
+final class APIManager{
+    
+    //MARK: Properties
+    
+    //MARK: Initializer
+    init() {
+    }
+    
+    //MARK: Methods
+    func getCharactersList()async throws ->Array<Character> {
+        guard let url = URL(string: "https://rickandmortyapi.com/api/character")
+        else {
+            print("Invalid URL")
+            return []
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        var res:Array<Character> = []
+        guard let jsonData = try? JSONSerialization.jsonObject(with: data) as? [String:Any],
+            let charactersInfo = jsonData["results"] as? [[String:Any]]
+        else{
+            print("Invalid data")
+            return []
+        }
+        for info in charactersInfo{
+            guard let name = info["name"] as? String,
+                  let status = info["status"] as? String,
+                  let type = info["type"] as? String,
+                  let gender = info["gender"] as? String,
+                  let origin = info["origin"] as? [String:String],
+                  let location = info["location"] as? [String:String],
+                  let originName = origin["name"],
+                  let locationName = location["name"],
+                  let date = info["created"] as? String,
+                  let dateCreated = self.convertDate(strdate: date)
+            else {
+                print("Invalid types")
+                return []
+            }
+            
+            guard let imageURLstring = info["image"] as? String,
+                  let imageURL = URL(string: imageURLstring),
+                  let image = self.getImage(url: imageURL)
+            else {
+                print("Invalid image URL")
+                return []
+            }
+            
+            let character = Character(name: name, status: status, type: type, gender: gender, originName: originName, locationName: locationName, image: image, dateCreated: dateCreated)
+            
+            res.append(character)
+        }
+        return res
+    }
+    
+    private func getImage(url: URL) -> UIImage?{
+        let data = try? Data(contentsOf: url)
+        if let data = data{
+            let image = UIImage(data: data)
+            return image
+        }
+        return nil
+    }
+    
+    private func convertDate(strdate: String)-> Date?{
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return formatter.date(from: strdate)
+    }
+}
